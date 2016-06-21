b0d6a26c80100c8dc63303c5ce15e4456a06ee94
tikhonov <4tikhonov@gmail.com>
2016-06-21 14:18:34 +0200

    Script to move dataset

---
+++ copydata.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp /home/webcrawler/Desktop/webcrawler_bigdata_may2016.xlsx /home/webcrawler/projects/
+/home/webcrawler/projects/webcrawler/Dataset.py
+cp /home/webcrawler/projects/webcrawler/finaldataset.xlsx /home/webcrawler/Desktop/finaldataset.xlsx
